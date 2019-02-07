618537c8c82370faf46785249a485c42880c9a68
Ladlen <TwilightTower@mail.ru>
2019-02-08 01:18:38 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://uid2018.hosting1.b-leader.ru/admin/');
+define('HTTP_CATALOG', 'http://uid2018.hosting1.b-leader.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://uid2018.hosting1.b-leader.ru/admin/');
+define('HTTPS_CATALOG', 'http://uid2018.hosting1.b-leader.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/uid2018/data/www/uid2018.hosting1.b-leader.ru/admin/');
+define('DIR_SYSTEM', '/var/www/uid2018/data/www/uid2018.hosting1.b-leader.ru/system/');
+define('DIR_IMAGE', '/var/www/uid2018/data/www/uid2018.hosting1.b-leader.ru/image/');
+define('DIR_STORAGE', '/var/www/uid2018/data/www/storage/');
+define('DIR_CATALOG', '/var/www/uid2018/data/www/uid2018.hosting1.b-leader.ru/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'uid2018_admin');
+define('DB_PASSWORD', 'Y1v9D2j3');
+define('DB_DATABASE', 'uid2018_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ system/storage/cache/56/56e2cc2f73eae32d23065f8d8b62431826c3764a01a3ac5f53cfdc656cedeb67.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_30dfac45d87e1937e4b2b68be3fb6118b13a770ec44ee535816d119cab35595e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
